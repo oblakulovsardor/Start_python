ee745e5178bc896bc1d27271055ae4cbd8806369
sarikwell <oblakulov_sarik@mail.ru>
2021-02-14 02:09:37 +0500

    todays training

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+# # # 
+# # talaba_1={}
+# # talaba_1['ism']='Shohjahon'
+# # talaba_1['familiya']='imomov'
+# # talaba_1['kurs']=4
+# # print(talaba_1)
+# # print(f"Talaba {talaba_1['ism'].title()}, {talaba_1['kurs']}")
+
+# student_1={}
+# student_1['ism']=input('Talabaning ismini kiriting\n>>>')
+# student_1['familiya']=input('Talabaning familiyasini kiriting\n>>>')
+# student_1['kurs']=input('Talabaning kursini kiriting\n>>>')
+# student_1['yosh']=int(input('Talabaning Yoshini kiriting'))
+
+# print(student_1)
+# print(f'Student: {student_1['ism'].title()}')
